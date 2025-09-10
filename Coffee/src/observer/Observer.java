167d962086c761f3ae059c70e89cd994818fea7e
Guada7 <144471109+Guada7@users.noreply.github.com>
2025-09-10 09:45:59 -0500

    Add files via upload

---
+++ Coffee/src/observer/Observer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package observer;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface Observer {
+    void update(LoteCafe lote);
+}
---
+++ Coffee/src/subject/Subject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package subject;
+
+import observer.Observer;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface Subject {
+    void registerObserver(Observer o);
+    void removeObserver(Observer o);
+    void notifyObserver(Observer o);
+}
